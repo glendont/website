03dbed58f350144925d89440901af856560beb9c
Glendon Thaiw <glendon888@gmail.com>
2020-06-13 23:13:39 +0800

    Updates

---
+++ precache-manifest.ca0cf6a51f3b3019d63dcbc8702278c2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ba4e9c78b86566698f70c74eede9a1",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "9062dd74c2972ae9cf52",
+    "url": "/website/static/css/main.7859457a.chunk.css"
+  },
+  {
+    "revision": "3f4914ba5b4f712a2458",
+    "url": "/website/static/js/2.b394b1eb.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/website/static/js/2.b394b1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9062dd74c2972ae9cf52",
+    "url": "/website/static/js/main.5efbb9f2.chunk.js"
+  },
+  {
+    "revision": "e5b6a0b645d92d2d7b16",
+    "url": "/website/static/js/runtime-main.84cf5a30.js"
+  },
+  {
+    "revision": "d918aeda3a0b9cce9b1068766dc5a04b",
+    "url": "/website/static/media/CRS.d918aeda.gif"
+  },
+  {
+    "revision": "98313b59a7c00c2329d79c5f15e8fcd3",
+    "url": "/website/static/media/Glendon_ProfilePic.98313b59.png"
+  },
+  {
+    "revision": "c98883e56919a6bc067d0098ea853029",
+    "url": "/website/static/media/datasci.c98883e5.png"
+  },
+  {
+    "revision": "55f13e439a3ea9174cf71b43355975c4",
+    "url": "/website/static/media/intern_amazon.55f13e43.jpg"
+  },
+  {
+    "revision": "40900be6d2c2c6da4a198b90f407d7e3",
+    "url": "/website/static/media/intern_deloitte.40900be6.jpg"
+  },
+  {
+    "revision": "378efc6dfb75b6c7d75a6beee6e0cfa3",
+    "url": "/website/static/media/intern_dsta.378efc6d.jpg"
+  },
+  {
+    "revision": "442fba98c80a99079de53fc97f364dac",
+    "url": "/website/static/media/intern_seagate.442fba98.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
