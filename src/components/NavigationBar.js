f05f7c3ca6b7dbbd9f61542f1635f32c3694a314
Glendon Thaiw <glendon888@gmail.com>
2020-07-01 10:47:52 +0800

    Fixed Navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,7 +6,7 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
 import LocalPhoneIcon from "@material-ui/icons/LocalPhone";
-
+import { useLocation } from "react-router-dom";
 const onClick = () => {
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
@@ -49,66 +49,70 @@ const onClick = () => {
   }
 };
 
-const NavigationBar = () => (
-  <nav className="navbar" style={{ backgroundColor: "#2F4049" }}>
-    <ul className="nav-links">
-      <li className="nav-links-homepage">
-        <Link to="/website">Home</Link>{" "}
-      </li>
-      <li className="nav-links-projects">
-        <Link to="/projects">Projects</Link>
-      </li>
-      <li className="nav-links-resume">
-        <a
-          href="https://www.keepandshare.com/doc7/62364/resume-of-glendon-1-pdf-38k?da=y"
-          target="_blank"
-        >
-          Resume
-        </a>
-      </li>
-      <li className="nav-links-contact">
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-
-    <div className="burger" onClick={onClick}>
-      <div className="line1"> </div>
-      <div className="line2"> </div>
-      <div className="line3"> </div>
-    </div>
-
-    <Fragment>
-      <ul className="navbar-logo">
-        <li>
-          <a href="https://www.linkedin.com/in/glendonthaiw/">
-            {" "}
-            <LinkedInIcon fontSize="medium" />{" "}
+const NavigationBar = () => {
+  return (
+    <nav className="navbar" style={{ backgroundColor: "#2F4049" }}>
+      <ul className="nav-links">
+        <li className="nav-links-home">
+          <Link to="/website" activeClassName="currentlyOn">
+            Home
+          </Link>{" "}
+        </li>
+        <li className="nav-links-projects">
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li className="nav-links-resume">
+          <a
+            href="https://www.keepandshare.com/doc7/62364/resume-of-glendon-1-pdf-38k?da=y"
+            target="_blank"
+          >
+            Resume
           </a>
         </li>
-
-        <li>
-          <a href="https://github.com/glendont">
-            {" "}
-            <GitHubIcon fontSize="small" />{" "}
-          </a>
-        </li>
-
-        <li>
-          <a href="mailto:glendon888@gmail.com">
-            {" "}
-            <AlternateEmailIcon fontSize="medium" />{" "}
-          </a>
-        </li>
-
-        <li>
-          <a href="/contact">
-            {" "}
-            <LocalPhoneIcon fontSize="medium" />{" "}
-          </a>
+        <li className="nav-links-contact">
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
-    </Fragment>
-  </nav>
-);
+
+      <div className="burger" onClick={onClick}>
+        <div className="line1"> </div>
+        <div className="line2"> </div>
+        <div className="line3"> </div>
+      </div>
+
+      <Fragment>
+        <ul className="navbar-logo">
+          <li>
+            <a href="https://www.linkedin.com/in/glendonthaiw/">
+              {" "}
+              <LinkedInIcon fontSize="medium" />{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="https://github.com/glendont">
+              {" "}
+              <GitHubIcon fontSize="small" />{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="mailto:glendon888@gmail.com">
+              {" "}
+              <AlternateEmailIcon fontSize="medium" />{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="/contact">
+              {" "}
+              <LocalPhoneIcon fontSize="medium" />{" "}
+            </a>
+          </li>
+        </ul>
+      </Fragment>
+    </nav>
+  );
+};
 
 export default NavigationBar;
